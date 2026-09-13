2e6dbe0ccb19426d4c64e15ac01f9be395f41cd8
EdiMena <edison14_93@hotmail.com>
2026-09-13 08:14:23 -0500

    feat account: add movement repository

---
+++ Devsu.Accounts/src/Application/Abstractions/IMovementRepository.cs
@@ -0,0 +1,9 @@
+using Domain;
+
+namespace Application.Abstractions;
+
+public interface IMovementRepository
+{
+    Task<Movement> GetByIdAsync(int movementId);
+    Task<IReadOnlyList<Movement>> GetByAccountAsync(string accountNumber);
+}
