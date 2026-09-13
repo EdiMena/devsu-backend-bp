58bd2a29c1f068030742cf62007105d2214c540e
EdiMena <edison14_93@hotmail.com>
2026-09-13 07:36:33 -0500

    feat account: add account repository.

---
+++ Devsu.Accounts/src/Application/Abstractions/IAccountRepository.cs
@@ -0,0 +1,12 @@
+using Domain;
+
+namespace Application.Abstractions;
+
+public interface IAccountRepository
+{
+    Task<Account> GetByNumberAsync(string accountNumber);
+    Task<Account> GetByNumberWithMovementsAsync(string accountNumber);
+    Task<IReadOnlyList<Account>> GetAllAsync();
+    Task AddAsync(Account account);
+    Task UpdateAsync(Account account);
+}
