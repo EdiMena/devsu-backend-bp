aeee10a4e2b2f3fdd38b2daedf13a974c05a4b16
EdiMena <edison14_93@hotmail.com>
2026-09-13 08:17:30 -0500

    feat clients: update methods nullable

--- Devsu.Clients/src/Application/Abstractions/IClientRepository.cs
+++ Devsu.Clients/src/Application/Abstractions/IClientRepository.cs
@@ -4,7 +4,7 @@ namespace Application.Abstractions;
 
 public interface IClientRepository
 {
-    Task<Client> GetByIdAsync(int id);
+    Task<Client?> GetByIdAsync(int id);
     Task<IReadOnlyList<Client>> GetAllAsync();
     Task AddAsync(Client client);
     Task UpdateAsync(Client client);
