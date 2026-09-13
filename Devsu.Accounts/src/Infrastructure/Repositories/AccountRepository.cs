1414f177f405710e9a8772d877c155d48adddb5e
EdiMena <edison14_93@hotmail.com>
2026-09-13 08:48:11 -0500

    fix account: validation logic in GetByNumberAsync

--- Devsu.Accounts/src/Infrastructure/Repositories/AccountRepository.cs
+++ Devsu.Accounts/src/Infrastructure/Repositories/AccountRepository.cs
@@ -11,11 +11,12 @@ public class AccountRepository : IAccountRepository
     public AccountRepository(AccountsDbContext context) => _context = context;
 
     public async Task<Account?> GetByNumberAsync(string accountNumber) => await _context.Accounts
-        .Include(a => a.Movements.OrderByDescending(m => m.MovementId).Take(1)).FirstOrDefaultAsync();
+        .Include(a => a.Movements.OrderByDescending(m => m.MovementId).Take(1))
+        .FirstOrDefaultAsync(a => a.AccountNumber == accountNumber);
 
     public async Task<Account?> GetByNumberWithMovementsAsync(string accountNumber) => await _context.Accounts
         .Include(a => a.Movements).FirstOrDefaultAsync(a => a.AccountNumber == accountNumber);
-    
+
     public async Task<IReadOnlyList<Account>> GetAllAsync() => await _context.Accounts.ToListAsync();
 
     public async Task AddAsync(Account account)
@@ -23,6 +24,6 @@ public class AccountRepository : IAccountRepository
         _context.Accounts.Add(account);
         await _context.SaveChangesAsync();
     }
-    
+
     public async Task UpdateAsync(Account account) => await _context.SaveChangesAsync();
 }
