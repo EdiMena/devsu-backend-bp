97b5596b2ce1b455c89527a8ede4444fa96c7bbe
EdiMena <edison14_93@hotmail.com>
2026-09-13 08:56:41 -0500

    feat accounts: add repository services

--- Devsu.Accounts/src/Api/Program.cs
+++ Devsu.Accounts/src/Api/Program.cs
@@ -1,10 +1,15 @@
+using Application.Abstractions;
 using Infrastructure.Persistence;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AccountsDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("AccountsDb")).UseSnakeCaseNamingConvention());
+builder.Services.AddDbContext<AccountsDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AccountsDb")).UseSnakeCaseNamingConvention());
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IMovementRepository, MovementRepository>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
